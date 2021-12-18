aff7e09c65cb99c8ee6ae416ec39cb639f6e23a9
Adithya <86290692+adithyahere01@users.noreply.github.com>
2021-12-18 11:37:47 +0530

    Update addTwoNumbers-forward.py

--- Sum_linkedlist/addTwoNumbers-forward.py
+++ Sum_linkedlist/addTwoNumbers-forward.py
@@ -1,22 +1,26 @@
  def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         s1,s2=0,0
+        #converting it to Numbers
         while l1:
                 s1=s1*10+l1.val
                 l1=l1.next
         while l2:
                 s2=s2*10+l2.val
                 l2=l2.next
-                
+        
+        #getting sum
         sum_ = s1 + s2
-        dummylist=dummy=ListNode(0)
+        dummylist=dummy=ListNode(0) #creating new listnode for result
         if sum_ == 0:
             return dummy
         
+        #converting Result number to linkeldlist
         while sum_>0:
             dummy.next = ListNode(sum_ % 10)
             dummy = dummy.next
             sum_ //= 10
             
+        #reversing the Result(bcoz it is forward order)
         prev = None
         dummy = dummylist.next
         while dummy:
