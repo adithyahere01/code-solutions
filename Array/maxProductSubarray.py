d47e8f8abd38b0f19e4f8770cfc6716276896714
Adithya <adithyaishr@gmail.com>
2022-01-28 10:34:48 +0530

    🍀

---
+++ Array/maxProductSubarray.py
@@ -0,0 +1,11 @@
+ def maxProduct(self, nums):
+        curMax, curMin = 1, 1
+        res = nums[0]
+        
+        for n in nums:
+            vals = (n, n * curMax, n * curMin)
+            curMax, curMin = max(vals), min(vals)
+			
+            res = max(res, curMax)
+            
+        return res
